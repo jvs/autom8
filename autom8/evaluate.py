26c1e34bfe8ba97b731f788fc2413c8004d0c58e
jvs <vonseg@gmail.com>
2018-10-29 21:05:32 -0500

    Restore "WIP: Try adding some statistics."
    This reverts commit f31e9a8ac85d102d6d15117064f69fb003445655,
    and restores commit 83ec6331cdc7060abd2ef4b79833da10ffe7d9a4.

--- autom8/evaluate.py
+++ autom8/evaluate.py
@@ -1,11 +1,15 @@
 from collections import namedtuple
+
 import numpy as np
+import pandas as pd
 import sklearn.metrics
+import scipy
+
 from .exceptions import expected
 
 
 Evaluation = namedtuple('Evaluation',
-    'problem_type, test_indices, derived_columns, train, test',)
+    'problem_type, test_indices, derived_columns, train, test, stats')
 
 EvaluationSection = namedtuple('EvaluationSection',
     'predictions, probabilities, metrics')
@@ -18,6 +22,7 @@ def evaluate_pipeline(ctx, pipeline):
         derived_columns=ctx.matrix.formulas,
         train=_evaluate_predictions(ctx, pipeline, *ctx.training_data()),
         test=_evaluate_predictions(ctx, pipeline, *ctx.testing_data()),
+        stats=_calculate_stats(ctx, pipeline),
     )
 
 
@@ -138,3 +143,63 @@ def _nonzero_root_columns(ctx, pipeline):
         c for c, w in zip(columns, weights) if w != 0
     ]
     return {i for c in nonzeros for i in c.root_columns()}
+
+
+def _calculate_stats(ctx, pipeline):
+    if not ctx.is_regression:
+        return None
+
+    result = {}
+    try:
+        # Get our predictions for the whole dataset.
+        X = ctx.matrix.stack_columns(float)
+        y = ctx.labels.encoded
+        y_hat = pipeline._predict(X).predictions
+
+        # Calculate standard squared error.
+        sse = np.sum((y_hat - y) ** 2)
+        result['sse'] = sse
+
+        # Get the mean squared error.
+        mse = sklearn.metrics.mean_squared_error(y, y_hat)
+        result['mse'] = mse
+
+        # Keep n handy. (Used by f_stat, coef_se, coef_pval.)
+        n = X.shape[0]
+
+        # Calculate summary F-statistic for beta coefficients.
+        p = X.shape[1]
+        r2 = sklearn.metrics.r2_score(y, y_hat)
+        f_stat = (r2 / p) / ((1 - r2) / (n - p - 1))
+        result['f_stat'] = f_stat
+
+        # Calculate standard error for beta coefficients.
+        X1 = np.hstack((np.ones((n, 1)), np.matrix(X)))
+        inv = np.linalg.inv(X1.T * X1)
+        matrix = scipy.linalg.sqrtm(mse * inv)
+        coef_se = np.diagonal(matrix)
+
+        # If we got a bunch of complex numbers, then just stop here.
+        num_complex = sum(1 for i in coef_se if isinstance(i, complex))
+        too_many = len(coef_se) / 4
+        if num_complex > too_many:
+            return result
+
+        result['coef_se'] = coef_se
+
+        # Calculate t-statistic for beta coefficients.
+        est = pipeline.estimator
+        a = np.array(est.intercept_ / coef_se[0])
+        b = np.array(est.coef_ / coef_se[1:])
+        coef_tval = np.append(a, b)
+        result['coef_tval'] = coef_tval
+
+        # Calculate p-values for beta coefficients.
+        coef_pval = 2 * (1 - scipy.stats.t.cdf(abs(coef_tval), n - 1))
+        result['coef_pval'] = coef_pval
+    except Exception:
+        # For now, just silently ignore it. It usually means that the estimator
+        # doesn't have the required "coef_" and "intercept_" attributes.
+        pass
+    finally:
+        return result or None
